372b6e44d51bfe3caad9ff18d124387cb72467ae
Pepijn dik <pepijn@pdik.nl>
2023-02-26 15:54:50 +0100

    Updated Manifest

--- app/src/main/java/nl/pdik/level6/task1/MainActivity.kt
+++ app/src/main/java/nl/pdik/level6/task1/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import nl.pdik.level6.task1.ui.theme.Task1Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +20,32 @@ class MainActivity : ComponentActivity() {
         setContent {
             Task1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Level6Task1App()
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Level6Task1App() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) }
+            )
+        }
+    ) { innerPadding ->
+        TestText(navController, modifier = Modifier.padding(innerPadding))
+    }
+
 }
 
-@Preview(showBackground = true)
+/**
+ * You can see this as a nav_graph.xml in compose
+ */
 @Composable
-fun DefaultPreview() {
-    Task1Theme {
-        Greeting("Android")
-    }
+fun TestText(navController: NavHostController, modifier: Modifier = Modifier) {
+    //TODO: we change this code later
+    Text("It works! - remove this text later")
 }
